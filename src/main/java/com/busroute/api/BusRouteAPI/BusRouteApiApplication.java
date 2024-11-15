5f4c5f0e91caadf71c33297f9a0f1fe4980d3369
Om Tamanag <np03cs4s220426@heraldcollege.edu.np>
2024-11-15 09:37:30 +0545

    Created a new Project

---
+++ src/main/java/com/busroute/api/BusRouteAPI/BusRouteApiApplication.java
@@ -0,0 +1,13 @@
+package com.busroute.api.BusRouteAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusRouteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusRouteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busroute/api/BusRouteAPI/BusRouteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busroute.api.BusRouteAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusRouteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
