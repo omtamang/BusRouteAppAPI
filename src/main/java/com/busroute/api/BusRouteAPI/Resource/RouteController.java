5e070c0d5b2f08d85faddc23b0dfcb4ff9617091
Om Tamanag <np03cs4s220426@heraldcollege.edu.np>
2025-03-18 18:16:20 +0545

    Added some controllers and edit delete of route

--- src/main/java/com/busroute/api/BusRouteAPI/Resource/RouteController.java
+++ src/main/java/com/busroute/api/BusRouteAPI/Resource/RouteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,6 +42,27 @@ public class RouteController {
     public ResponseEntity<Integer> deleteRoute(@PathVariable int routeId){
         routeRepository.deleteById(routeId);
         return ResponseEntity.status(204).body(routeId);
+    }
+
+    @PutMapping("/route/{routeId}")
+    public ResponseEntity<Route> updateRoute(@PathVariable int routeId, @RequestBody Route updatedRoute){
+        Optional<Route> existingRoute = routeRepository.findById(routeId);
+
+        if(existingRoute.isPresent()){
+            Route route = existingRoute.get();
+            route.setRoute_name(updatedRoute.getRoute_name());
+            route.setStart_lat(updatedRoute.getStart_lat());
+            route.setStart_lng(updatedRoute.getStart_lng());
+            route.setEnd_lat(updatedRoute.getEnd_lat());
+            route.setEnd_lng(updatedRoute.getEnd_lng());
+            route.setNo_of_buses(updatedRoute.getNo_of_buses());
+            route.setStops(updatedRoute.getStops());
+
+            routeRepository.save(route);
+            return ResponseEntity.ok(route);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
